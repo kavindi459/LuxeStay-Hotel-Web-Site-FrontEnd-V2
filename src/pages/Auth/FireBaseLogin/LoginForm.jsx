e2a906d4fb9d531deeda6fb399944333ff229d5e
Mayuru Madhuranga <mayuru@cypsolabs.com>
2025-06-09 00:19:21 +0530

    bug fix

--- src/pages/Auth/FireBaseLogin/LoginForm.jsx
+++ src/pages/Auth/FireBaseLogin/LoginForm.jsx
@@ -69,7 +69,6 @@ const LoginPages = () => {
       navigate("/");
     } catch (error) {
       toast.error(`Google Login Failed: ${error.message}`);
-      
     } finally {
       
       setTimeout(() => setLoading(false), 300);
